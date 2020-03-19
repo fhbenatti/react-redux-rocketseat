862ac70a068c032b1b695843c04c9b5e68d3eae8
Fábio Benatti <fhbenatti@gmail.com>
2020-03-19 13:55:18 -0300

    Initial commit

---
+++ src/components/Video/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Video extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <strong>Módulo X</strong>
+        <span>Aula X</span>
+      </div>
+    )
+  }
+}
+
+export default Video
